2a8fc7ad5ff60fd56f56e26e1da983b554f5b7cc
Patrick Schuur <patrickschuurdev@gmail.com>
2025-01-15 17:50:07 +0100

    Window with glfw, vulkan and glm

---
+++ VulkanTesting/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#define GLFW_INCLUDE_VULKAN
+#include <glfw/glfw3.h>
+#include <glm/glm.hpp>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+
+	if (!glfwInit()) {
+		throw std::runtime_error("Failed to initialize GLFW");
+	}
+	
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, World!", nullptr, nullptr);
+	if (!window) {
+		glfwTerminate();
+	}
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << extensionCount << " extensions supported\n";
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	return 0;
+}
