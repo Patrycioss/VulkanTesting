08a30019dd703debc38b6596161104177b9795ca
Patrick Schuur <patrickschuurdev@gmail.com>
2025-01-16 21:58:15 +0100

    Moved logging to pure macros

--- VulkanTesting/utils/log.hpp
+++ VulkanTesting/utils/log.hpp
@@ -1,18 +1,5 @@
 ﻿#pragma once
-
-#define UTIL_THROW(message) Log::Throw(__FILE__, __LINE__, message)
-#define UTIL_LOG(message) Log::Log(__FILE__, __LINE__, message)
-
 #include <iostream>
 
-namespace Log
-{
-	inline void Throw(const char* fileName, const int line, const std::string& message) {
-		auto exceptionMessage = std::string(fileName) + "[" + std::to_string(line) + "]: " + message + "\n";
-		throw std::runtime_error(exceptionMessage.c_str());
-	}
-
-	inline void Log(const char* fileName, const int line, const std::string& message) {
-		std::cout << fileName << " " << line << ": " << message << std::endl;
-	}
-}
+#define UTIL_THROW(message) throw std::runtime_error(std::string(__FILE__) + "[" + std::to_string(__LINE__) + "]: " + message + "\n")
+#define UTIL_LOG(message) std::cout << fileName << " " << line << ": " << message << std::endl
