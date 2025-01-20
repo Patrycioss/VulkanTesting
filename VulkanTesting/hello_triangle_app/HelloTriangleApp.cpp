6a0fe729e688f7ad1e708ca6eb2142a8a06bca4e
Patrick Schuur <patrickschuurdev@gmail.com>
2025-01-20 18:02:27 +0100

    Finish record command buffer function

--- VulkanTesting/hello_triangle_app/HelloTriangleApp.cpp
+++ VulkanTesting/hello_triangle_app/HelloTriangleApp.cpp
@@ -808,12 +808,49 @@ void HelloTriangleApp::createCommandBuffer() {
 	}
 }
 
-void HelloTriangleApp::recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex) {
+void HelloTriangleApp::recordCommandBuffer(const VkCommandBuffer commandBuffer, const uint32_t imageIndex) {
 	VkCommandBufferBeginInfo beginInfo{};
 	beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
 	VkResult commandBufferBeginResult = vkBeginCommandBuffer(commandBuffer, &beginInfo);
 	if (commandBufferBeginResult != VK_SUCCESS) {
 		UTIL_THROW("Failed to begin recording command buffer!");
+	}
+
+	VkRenderPassBeginInfo renderPassInfo{};
+	renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+	renderPassInfo.renderPass = renderPass;
+	renderPassInfo.framebuffer = swapChainFramebuffers[imageIndex];
+	renderPassInfo.renderArea.offset = {0, 0};
+	renderPassInfo.renderArea.extent = swapChainExtent;
+
+	constexpr VkClearValue clearColor = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
+	renderPassInfo.clearValueCount = 1;
+	renderPassInfo.pClearValues = &clearColor;
+
+	vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+	vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+
+	VkViewport viewport{};
+	viewport.x = 0.0f;
+	viewport.y = 0.0f;
+	viewport.width = static_cast<float>(swapChainExtent.width);
+	viewport.height = static_cast<float>(swapChainExtent.height);
+	viewport.minDepth = 0.0f;
+	viewport.maxDepth = 1.0f;
+	vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+	VkRect2D scissor{};
+	scissor.offset = {0, 0};
+	scissor.extent = swapChainExtent;
+	vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+	vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+
+	vkCmdEndRenderPass(commandBuffer);
+
+	const VkResult endCommandBufferResult = vkEndCommandBuffer(commandBuffer);
+	if (endCommandBufferResult != VK_SUCCESS) {
+		UTIL_THROW("Failed to end recording command buffer!");
 	}
 }
